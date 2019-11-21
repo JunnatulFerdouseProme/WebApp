7f1cbc68ca6d26ea5da53c15eebeeda8834f93da
junnatul.ferdouse@gmail.com <junnatul.ferdouse@gmail.com>
2019-11-21 18:09:47 +0600

    Add project files.

---
+++ WebApp/Controllers/StudentController.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Routing.Constraints;
+using  WebApp.Model.Model;
+using  WebApp.BLL.BLL;
+
+using WebApp.Models;
+using AutoMapper;
+
+namespace WebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentManager _studentManager = new StudentManager();
+        DepartmentManager _departmentManager = new DepartmentManager();
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+           StudentViewModel studentViewModel = new StudentViewModel();
+
+           studentViewModel.Students = _studentManager.GetAll();
+           studentViewModel.DepartmentSelectListItems = _departmentManager
+                                                        .GetAll()
+                                                        .Select(c=> new SelectListItem()
+                                                        {
+                                                            Value = c.Id.ToString(), Text = c.Name
+                                                        }).ToList();
+
+
+            return View(studentViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Add(StudentViewModel studentViewModel)
+        {
+
+            string message = "";
+            //Student student = new Student();
+
+            //student.RollNo = studentViewModel.RollNo;
+            //student.Name = studentViewModel.Name;
+            //student.Address = studentViewModel.Address;
+            //student.Age = studentViewModel.Age;
+            //student.DepartmentId = studentViewModel.DepartmentId;
+            if (ModelState.IsValid)
+            {
+                Student student = Mapper.Map<Student>(studentViewModel);
+
+                if (_studentManager.Add(student))
+                {
+                    message = "Saved";
+                }
+                else
+                {
+                    message = "Not Saved";
+                }
+            }
+            else
+            {
+                message = "ModelState is invalied!";
+            }
+           
+
+            
+
+            ViewBag.Message = message;
+            studentViewModel.Students = _studentManager.GetAll();
+            studentViewModel.DepartmentSelectListItems = _departmentManager
+                .GetAll()
+                .Select(c => new SelectListItem()
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }).ToList();
+            return View(studentViewModel);
+        }
+
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            StudentViewModel studentViewModel = new StudentViewModel();
+
+            studentViewModel.Students = _studentManager.GetAll();
+            studentViewModel.DepartmentSelectListItems = _departmentManager
+                .GetAll()
+                .Select(c => new SelectListItem()
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }).ToList();
+
+
+            return View(studentViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Search(StudentViewModel studentViewModel)
+        {
+           
+            var students = _studentManager.GetAll();
+
+            if (studentViewModel.RollNo != null)
+            {
+               students =students.Where(c => c.RollNo.Contains(studentViewModel.RollNo)).ToList();
+            }
+            if (studentViewModel.Name != null)
+            {
+                students = students.Where(c => c.Name.ToUpper().Contains(studentViewModel.Name.ToUpper())).ToList();
+            }
+
+            studentViewModel.Students = students;
+            studentViewModel.DepartmentSelectListItems = _departmentManager
+                .GetAll()
+                .Select(c => new SelectListItem()
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }).ToList();
+
+
+            return View(studentViewModel);
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var student = _studentManager.GetById(id);
+
+            StudentViewModel studentViewModel = Mapper.Map<StudentViewModel>(student);
+
+            studentViewModel.Students = _studentManager.GetAll();
+            studentViewModel.DepartmentSelectListItems = _departmentManager
+                                                         .GetAll()
+                                                         .Select(c => new SelectListItem()
+                                                         {
+                                                             Value = c.Id.ToString(),
+                                                             Text = c.Name
+                                                         }).ToList();
+
+
+            return View(studentViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(StudentViewModel studentViewModel)
+        {
+
+            string message = "";
+            //Student student = new Student();
+
+            //student.RollNo = studentViewModel.RollNo;
+            //student.Name = studentViewModel.Name;
+            //student.Address = studentViewModel.Address;
+            //student.Age = studentViewModel.Age;
+            //student.DepartmentId = studentViewModel.DepartmentId;
+            if (ModelState.IsValid)
+            {
+                Student student = Mapper.Map<Student>(studentViewModel);
+
+                if (_studentManager.Update(student))
+                {
+                    message = "Updated";
+                }
+                else
+                {
+                    message = "Not Updated";
+                }
+            }
+            else
+            {
+                message = "ModelState is invalied!";
+            }
+
+
+
+
+            ViewBag.Message = message;
+            studentViewModel.Students = _studentManager.GetAll();
+            studentViewModel.DepartmentSelectListItems = _departmentManager
+                .GetAll()
+                .Select(c => new SelectListItem()
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }).ToList();
+            return View(studentViewModel);
+        }
+    }
+}
